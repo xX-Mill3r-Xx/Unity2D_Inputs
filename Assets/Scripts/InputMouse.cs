a5b7bb653df85c0ad5146fc68d380663711ad9e8
MillerVasco <millermartinsvasco1@gmail.com>
2023-03-07 15:06:02 -0300

    1505

--- Assets/Scripts/InputMouse.cs
+++ Assets/Scripts/InputMouse.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class InputMouse : MonoBehaviour
 {
+    private float Move_H;
+    private float Move_V;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MouseInput_Move();
+        MouseInput_Scroll();
+        MouseInput_Click();
+    }
+
+    void MouseInput_Move()
+    {
+        //Aqui podemos simular um mouse ou o movimento de um mouse para games Point Click
+        Move_H = Input.GetAxis("Mouse X");
+        Move_V = Input.GetAxis("Mouse Y");
+        transform.Translate(new Vector3(Move_H, Move_V, 0));
+    }
+
+    void MouseInput_Scroll()
+    {
+        //Aqui podemos simular o movimento pelo scroll do mouse;
+        Move_H = Input.GetAxis("Mouse ScrollWheel");
+        transform.Translate(new Vector3(Move_H, 0, 0));
+    }
+
+    void MouseInput_Click()
+    {
+        //Aqui podemos simular movimento clicando com os botoes do mouse;
+        Move_H = Input.GetAxis("Fire1");
+        Move_V = Input.GetAxis("Fire2");
+        transform.Translate(new Vector3(Move_H * Time.deltaTime, Move_V * Time.deltaTime, 0));
     }
 }
--- Assets/Scripts/InputTeclado2.cs
+++ Assets/Scripts/InputTeclado2.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class InputTeclado2 : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    private float Move_H;
+    private float Move_V;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,15 @@ public class InputTeclado2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        AxisInput();
+    }
+
+    void AxisInput()
+    {
+        //Uma forma mais aprimorada de criar um codigo para capturar os comandos do teclado
+        Move_H = Input.GetAxis("Horizontal") * speed;
+        Move_V = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(new Vector3(Move_H * Time.deltaTime, Move_V * Time.deltaTime, 0));
     }
 }
